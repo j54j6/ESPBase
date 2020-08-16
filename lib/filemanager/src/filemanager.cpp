efde5fb36fac2a84e3ccfbb47a6d3b51739d82cc
Justin <mail@justinritter.de>
2020-08-16 21:27:13 +0200

    Update filemanager.cpp
    Add zeroData JsonWriter

--- lib/filemanager/src/filemanager.cpp
+++ lib/filemanager/src/filemanager.cpp
@@ -1117,11 +1117,18 @@ bool Filemanager::writeJsonFile(const char* Filename,  const char* jsonPattern[]
     if(!checkForInit())
     {
         #ifdef J54J6_LOGGING_H //use logging Libary if included
-            logger::SFLog(className, "getCreationTime", "Error Init. Check!", 2);
+            logger::SFLog(className, "writeJsonFile", "Error Init. Check!", 2);
         #endif
         return false;
     }
 
+    if(amountOfData == 0)
+    {
+        #ifdef J54J6_LOGGING_H //use logging Libary if included
+            logger::SFLog(className, "writeJsonFile", "no Data given (AmountData = 0) - SKIP", 1);
+        #endif
+        return true;
+    }
     const size_t capacity = JSON_OBJECT_SIZE(25) + 400;
     DynamicJsonDocument jsonDocument(capacity);
     String jsonOutput;
