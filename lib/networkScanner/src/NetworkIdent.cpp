4bd04e58dd653b44455f0e26a5693f68d920fd4e
Justin <mail@justinritter.de>
2020-08-18 21:23:07 +0200

    Add NetworkIdent
    Add NetworkIdent

--- lib/networkScanner/src/NetworkIdent.cpp
+++ lib/networkScanner/src/NetworkIdent.cpp
@@ -1,8 +1,126 @@
 #include "NetworkIdent.h"
 
+//Constructor
+NetworkIdent::NetworkIdent(WiFiManager* wifiManager, Filemanager* FM, const char* deviceName, int port)
+{
+    this->deviceIdentName = deviceName;
+    this->port = port;
+    this->wifiManager = wifiManager;
+    this->FM = FM;
+}
+
+
+//get Stuff
+const char* NetworkIdent::getDeviceIdentName()
+{
+    return this->deviceIdentName;
+}
+
+int NetworkIdent::getLocalOutPort()
+{
+    return udpHandler.localPort();
+}
+
+udpPacketResolve* NetworkIdent::getLastUDPPacketLoop()
+{
+    return &lastContent;
+}
+
+//control
+bool NetworkIdent::begin(int port)
+{
+    if(udpHandler.begin(this->port) == 0)
+    {
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            logging.SFLog(className, "begin", "Can't start UDP - begin() return false!", 2);
+        #endif
+        error.error = true;
+        error.ErrorCode = 421;
+        error.message = "Can't start UDP - UDP.begin() return false (0)!";
+        error.priority = 5;
+        return false;
+    }
+    udpListenerStarted = true;
+    return true;
+}
+
+void NetworkIdent::stop()
+{
+    udpListenerStarted = false;
+    udpHandler.stop();
+}
+
+
+bool NetworkIdent::checkForService(const char* serviceName)
+{
+    if(FM->begin())
+    {
+        if(!FM->fExist(serviceListPath))
+        {
+            #ifdef J54J6_LOGGING_H
+                logger logging;
+                logging.SFLog(className, "checkForService", "Servicelist File doesn't exist! - return False", 1);
+            #endif
+            return false;
+        }
+        
+        DynamicJsonDocument serviceList(425);
+        serviceList = FM->readJsonFile(serviceListPath);
+
+        if(!FM->error)
+        {
+            return serviceList.containsKey(serviceName);
+        }
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            logging.SFLog(className, "checkForService", "Error while read JSON File -> FM -> ArduinoJson has Error!");
+        #endif
+        return false;
+        
+    }
+}
+
+bool NetworkIdent::addService(const char* newServiceName, int usedPort)
+{
+    if(checkForService(newServiceName))
+    {
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            logging.SFLog(className, "addService", "Can't add Service - already exist or other Error - see above at Info");
+        #endif
+        return false;
+    }
+    
+    
+}
 
 
 void NetworkIdent::searchForDeviceName(const char* deviceName)
 {
-    udpHandler.
+    
+}
+
+
+//loop
+void NetworkIdent::loop()
+{
+    if(wifiManager->getWiFiState() == WL_CONNECTED && udpListenerStarted == true)
+    {
+        int packetSize = udpHandler.parsePacket();
+        if(packetSize)
+        {
+            char buffer[512];
+            lastContent.paketSize = packetSize;
+            lastContent.remoteIP = udpHandler.remoteIP();
+            lastContent.remotePort = udpHandler.remotePort();
+
+            int numbersReaded = udpHandler.read(lastContent.udpContent, 512);
+            if(numbersReaded > 0)
+            {
+                lastContent.udpContent[numbersReaded] = 0;
+            }
+        }
+    }
+    return;
 }
--- lib/networkScanner/src/NetworkIdent.h
+++ lib/networkScanner/src/NetworkIdent.h
@@ -4,32 +4,63 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
+#include "logging.h"
+#include "errorHandler.h"
+#include "filemanager.h"
+#include "wifiManager.h"
 
-class NetworkIdent {
+struct udpPacketResolve {
+    IPAddress remoteIP;
+    int remotePort;
+    int paketSize;
+    char udpContent[512];
+};
+
+class NetworkIdent : public ErrorSlave {
     private:
         const char* className = "NetworkIdent";
         const char* deviceIdentName = "notSet";
-        const char* functionalities[];
+        const char* serviceListPath = "/config/networkIdent/services.json"; //saved as {serviceType, port}
+        int port;
+        bool udpListenerStarted = false;
 
+        IPAddress broadCastIP = IPAddress(255,255,255,255); //only for first Time later this Address will dynamically changed per subnet
         WiFiUDP udpHandler;
+        Filemanager* FM;
+        WiFiManager* wifiManager;
+
+        udpPacketResolve lastContent;
     public:
-        NetworkIdent(const char* deviceName, int numberServices, );
+        NetworkIdent(WiFiManager* wifiManager, Filemanager* FM, const char* deviceName, int port = 62309);
 
         //set stuff
 
         //get stuff
         const char* getDeviceIdentName();
-        
+        int getLocalOutPort();
+        udpPacketResolve* getLastUDPPacketLoop();
         
         //control
+        bool begin(int port = -1);
+        void stop();
+
+
+        bool checkForService(const char* serviceName);
+        bool addService(const char* newServiceName, int usedPort);
+        bool delService(const char* serviceName);
+        bool serviceAvail(const char* serviceName);
+
         void searchForDeviceName(const char* deviceName);
         void searchForFunctionalities(const char* functionalitiy);
 
-        void sendUdpMessage(const char* workload, 
+        void sendUdpMessage(const char* workload, IPAddress ip);
 
         void returnMyDeviceName();
         void returnMyFuntionalitiy();
         void returnMyIP();
+
+        //loop
+        void loop();
 };
 
 #endif
