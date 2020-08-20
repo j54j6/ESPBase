b50452b0167b043a09166b483ea0b6b3a8720474
Justin <mail@justinritter.de>
2020-08-20 21:52:21 +0200

    Add NetworkIdent (working UDP Send)
    V0.1 f working NetworkIdent (sendUDP work (look for service)

--- lib/networkScanner/src/NetworkIdent.cpp
+++ lib/networkScanner/src/NetworkIdent.cpp
@@ -9,6 +9,13 @@ NetworkIdent::NetworkIdent(WiFiManager* wifiManager, Filemanager* FM, const char
     this->FM = FM;
 }
 
+//set stuff
+void NetworkIdent::setDeviceName(const char* newDeviceName)
+{
+    this->deviceIdentName = newDeviceName;
+}
+
+
 
 //get Stuff
 const char* NetworkIdent::getDeviceIdentName()
@@ -29,6 +36,7 @@ udpPacketResolve* NetworkIdent::getLastUDPPacketLoop()
 //control
 bool NetworkIdent::begin(int port)
 {
+    createConfigFile();
     if(udpHandler.begin(this->port) == 0)
     {
         #ifdef J54J6_LOGGING_H
@@ -49,6 +57,61 @@ void NetworkIdent::stop()
 {
     udpListenerStarted = false;
     udpHandler.stop();
+}
+
+
+bool NetworkIdent::createConfigFile()
+{
+    FM->mount();
+    if(!FM->fExist(serviceListPath))
+    {
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            logging.SFLog(className, "createConfigFile", "Config File doesn't exist - try to create", 1);
+        #endif 
+
+        if(FM->createFile(serviceListPath))
+        {
+            #ifdef J54J6_LOGGING_H
+                logger logging;
+                logging.SFLog(className, "createConfigFile", "File created!");
+            #endif 
+        }
+        else
+        {
+            #ifdef J54J6_LOGGING_H
+                logger logging;
+                logging.SFLog(className, "createConfigFile", "Can't create File! - return false", 2);
+            #endif
+            return false;
+        }
+
+        if(FM->writeJsonFile(serviceListPath, serviceConfigBlueprint, 1))
+        {
+            #ifdef J54J6_LOGGING_H
+                logger logging;
+                logging.SFLog(className, "createConfigFile", "Config Fallback written - return true");
+            #endif
+            return true;
+        }
+        else
+        {
+            #ifdef J54J6_LOGGING_H
+                logger logging;
+                logging.SFLog(className, "createConfigFile", "Can't write in File!");
+            #endif
+            return false;
+        }
+        return false;  
+    }
+    else
+    {
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            logging.SFLog(className, "createConfigFile", "File already exist - SKIP!");
+        #endif
+    }
+    return false;
 }
 
 
@@ -74,11 +137,13 @@ bool NetworkIdent::checkForService(const char* serviceName)
         }
         #ifdef J54J6_LOGGING_H
             logger logging;
-            logging.SFLog(className, "checkForService", "Error while read JSON File -> FM -> ArduinoJson has Error!");
+            logging.SFLog(className, "checkForService", "Error while read JSON File -> FM -> ArduinoJson has Error!", 2);
         #endif
+        FM->error = false;
         return false;
         
     }
+    return false;
 }
 
 bool NetworkIdent::addService(const char* newServiceName, int usedPort)
@@ -92,25 +157,214 @@ bool NetworkIdent::addService(const char* newServiceName, int usedPort)
         return false;
     }
     
+    DynamicJsonDocument services(425);
+    services = FM->readJsonFile(serviceListPath);
+
+    if(FM->error)
+    {
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            logging.SFLog(className, "addService", "Error while read JSON File -> FM -> ArduinoJson has Error!", 2);
+        #endif
+        FM->error = false;
+        return false;
+    }
+    services.add(newServiceName);
+    services.add(usedPort);
+    FM->writeJsonFile(serviceListPath, services);
     
+    if(checkForService(newServiceName))
+    {
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            logging.SFLog(className, "addService", "Service successfully added");
+        #endif
+        return true;
+    }
+    else
+    {
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            logging.SFLog(className, "addService", "Can't add Service - Service not  found after Add!", 2);
+        #endif
+        return false;
+    }
+    return false;
+}
+
+
+bool NetworkIdent::delService(const char* serviceName)
+{
+    if(checkForService(serviceName))
+    {
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            logging.SFLog(className, "delService", "Service exist - try to delete it");
+        #endif
+        DynamicJsonDocument services(425);
+        services = FM->readJsonFile(serviceListPath);
+
+        if(FM->error)
+        {
+            #ifdef J54J6_LOGGING_H
+                logger logging;
+                logging.SFLog(className, "delService", "Error while read JSON File -> FM -> ArduinoJson has Error!", 2);
+            #endif
+            FM->error = false;
+            return false;
+        }
+
+        services.remove(serviceName);
+        if(FM->writeJsonFile(serviceListPath, services))
+        {
+            #ifdef J54J6_LOGGING_H
+                logger logging;
+                logging.SFLog(className, "delService", "Service deleted!", 0);
+            #endif
+            return true;
+        }
+        else
+        {
+            #ifdef J54J6_LOGGING_H
+                logger logging;
+                logging.SFLog(className, "delService", "Can't write back File!", 2);
+            #endif
+            return false;
+        }
+        
+    }
+    return false;
 }
 
 
 void NetworkIdent::searchForDeviceName(const char* deviceName)
 {
+    if(wifiManager->getWiFiState() == WL_CONNECTED)
+    {
+        String message = "{\"deviceNameSearch\": \"";
+        message += deviceName;
+        message += "\"}";
+
+        udpHandler.beginPacket(broadcastIP, networkIdentPort);
+        udpHandler.write(message.c_str());
+        udpHandler.endPacket();
+    }
+    else
+    {
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            logging.SFLog(className, "searchForDeviceName", "Can't send UDP Message - no Network!", 2);
+        #endif
+    }
+}
+
+void NetworkIdent::searchForService(const char* serviceName)
+{
+    if(wifiManager->getWiFiState() == WL_CONNECTED)
+    {
+        String message = "{\"deviceServiceSearch\": \"";
+        message += serviceName;
+        message += "\"}";
+
+        udpHandler.beginPacket(broadcastIP, networkIdentPort);
+        udpHandler.write(message.c_str());
+        udpHandler.endPacket();
+    }
+    else
+    {
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            logging.SFLog(className, "searchForService", "Can't send UDP Message - no Network!", 2);
+        #endif
+    }
+}
+
+
+void NetworkIdent::sendUdpMessage(const char* workload, IPAddress ip, int port)
+{
+    if(wifiManager->getWiFiState() == WL_CONNECTED)
+    {
+        udpHandler.beginPacket(ip, port);
+        udpHandler.write(workload);
+        udpHandler.endPacket();
+    }
+    else
+    {
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            logging.SFLog(className, "sendUdpMessage", "Can't send UDP Message - no Network!", 2);
+        #endif
+    }
+    
     
 }
 
+void NetworkIdent::returnMyDeviceName()
+{
+    if(wifiManager->getWiFiState() == WL_CONNECTED)
+    {
+        String message = "{\"Answer\": \"";
+        message += "\"TRUE\",";
+        message += "\"DeviceName\":\"";
+        message += this->deviceIdentName;
+        message += "\",";
+        message += "\"ip\":";
+        message += "\"";
+        message += wifiManager->getLocalIP();
+        message += "\"}";
+
+
+        udpHandler.beginPacket(broadcastIP, networkIdentPort);
+        udpHandler.write(message.c_str());
+        udpHandler.endPacket();
+    }
+    else
+    {
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            logging.SFLog(className, "searchForDeviceName", "Can't send UDP Message - no Network!", 2);
+        #endif
+    }
+}
+
+void NetworkIdent::returnMyIP()
+{
+    if(wifiManager->getWiFiState() == WL_CONNECTED)
+    {
+        String message = "{\"Answer\": \"";
+        message += "\"TRUE\",";
+        message += "\"IP\":";
+        message += "\"";
+        message += wifiManager->getLocalIP();
+        message += "\"}";
+
+
+        udpHandler.beginPacket(broadcastIP, networkIdentPort);
+        udpHandler.write(message.c_str());
+        udpHandler.endPacket();
+    }
+    else
+    {
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            logging.SFLog(className, "searchForDeviceName", "Can't send UDP Message - no Network!", 2);
+        #endif
+    }
+}
 
 //loop
 void NetworkIdent::loop()
 {
+    if(classDisabled)
+    {
+        return;
+    }
+
     if(wifiManager->getWiFiState() == WL_CONNECTED && udpListenerStarted == true)
     {
         int packetSize = udpHandler.parsePacket();
         if(packetSize)
         {
-            char buffer[512];
             lastContent.paketSize = packetSize;
             lastContent.remoteIP = udpHandler.remoteIP();
             lastContent.remotePort = udpHandler.remotePort();
@@ -123,4 +377,47 @@ void NetworkIdent::loop()
         }
     }
     return;
+}
+
+
+/*
+    Inherited overwritten functionalities
+*/
+void NetworkIdent::startClass()
+{
+    if(this->classDisabled)
+    {
+        this->classDisabled = false;
+        this->begin();
+    }
+    else
+    {
+        this->begin();
+    }
+}
+
+void NetworkIdent::stopClass()
+{
+    if(!this->classDisabled)
+    {
+        this->classDisabled = true;
+        this->loop();
+        Serial.println("locked!");
+        return;
+    }
+}
+
+void NetworkIdent::pauseClass()
+{
+    this->stopClass();
+}
+
+void NetworkIdent::restartClass()
+{
+    this->startClass();
+}
+
+void NetworkIdent::continueClass()
+{
+    this->startClass();
 }
--- lib/networkScanner/src/NetworkIdent.h
+++ lib/networkScanner/src/NetworkIdent.h
@@ -18,23 +18,29 @@ struct udpPacketResolve {
 
 class NetworkIdent : public ErrorSlave {
     private:
+        bool classDisabled = false;
+
         const char* className = "NetworkIdent";
         const char* deviceIdentName = "notSet";
         const char* serviceListPath = "/config/networkIdent/services.json"; //saved as {serviceType, port}
         int port;
         bool udpListenerStarted = false;
+        int networkIdentPort = 63547;
+        
+        const char* serviceConfigBlueprint[1][2] = {{"NetworkIdent", "63547"}}; //port of network scanner
 
-        IPAddress broadCastIP = IPAddress(255,255,255,255); //only for first Time later this Address will dynamically changed per subnet
+
+        IPAddress broadcastIP = IPAddress(255,255,255,255); //only for first Time later this Address will dynamically changed per subnet
         WiFiUDP udpHandler;
         Filemanager* FM;
         WiFiManager* wifiManager;
 
         udpPacketResolve lastContent;
     public:
-        NetworkIdent(WiFiManager* wifiManager, Filemanager* FM, const char* deviceName, int port = 62309);
+        NetworkIdent(WiFiManager* wifiManager, Filemanager* FM, const char* deviceName, int port = 63547);
 
         //set stuff
-
+        void setDeviceName(const char* newDeviceName);
         //get stuff
         const char* getDeviceIdentName();
         int getLocalOutPort();
@@ -45,22 +51,31 @@ class NetworkIdent : public ErrorSlave {
         void stop();
 
 
+        bool createConfigFile();
         bool checkForService(const char* serviceName);
         bool addService(const char* newServiceName, int usedPort);
         bool delService(const char* serviceName);
-        bool serviceAvail(const char* serviceName);
 
         void searchForDeviceName(const char* deviceName);
-        void searchForFunctionalities(const char* functionalitiy);
+        void searchForService(const char* service);
 
-        void sendUdpMessage(const char* workload, IPAddress ip);
+        void sendUdpMessage(const char* workload, IPAddress ip, int port);
 
         void returnMyDeviceName();
-        void returnMyFuntionalitiy();
+        //void returnMyServices();
         void returnMyIP();
 
         //loop
         void loop();
+
+        /*
+        Inherited ErrorHandling
+       */
+        void startClass();
+        void stopClass();
+        void pauseClass();
+        void restartClass();
+        void continueClass();
 };
 
 #endif
