0b8e49ff26b8a5decb6a2973147f5cf33816a6e2
Justin <mail@justinritter.de>
2020-09-06 02:34:13 +0200

    add externServiceHandler
    add basicFunctionalities like
    - manAdd
    - createConfig
    - syntaxCreator
    - autoAddService

--- lib/externServiceHandler/src/externServiceHandler.cpp
+++ lib/externServiceHandler/src/externServiceHandler.cpp
@@ -27,24 +27,173 @@ int ExternServiceHandler::getServiceDestPort(const char* serviceName)
 {
     if(FM->fExist(getFilename(serviceName).c_str()))
     {
-        
+        //service already added - readout File
+        if(FM->checkForKeyInJSONFile(getFilename(serviceName).c_str(), "port"))
+        {
+            return int(FM->readJsonFileValue(getFilename(serviceName).c_str(), "port"));
+        }        
+    }
+    else if(FM->fExist(getFilename(serviceName, true).c_str()))
+    {
+        if(FM->checkForKeyInJSONFile(getFilename(serviceName, true).c_str(), "port"))
+        {
+            return int(FM->readJsonFileValue(getFilename(serviceName).c_str(), "port"));
+        }
     }
     
+    #ifdef J54J6_LOGGING_H
+        logger logging;
+        logging.SFLog(className, "getServiceDestPort", "Can't find DestPort - search in network...", 0);
+    #endif
+    //if file not exist or key "port" is not defined in File search for service and recreate the File
+    networkIdent->searchForService(serviceName);
+    lastRequest.id = networkIdent->getLastGeneratedId();
+    lastRequest.createdAt = millis();
+    lastRequest.serviceName = serviceName;
+    lastRequest.searchType = 2;
+    return -1;
 }
 
 IPAddress ExternServiceHandler::getServiceIP(const char* serviceName)
 {
+    if(FM->fExist(getFilename(serviceName).c_str()))
+    {
+        //service already added - readout File
+        if(FM->checkForKeyInJSONFile(getFilename(serviceName).c_str(), "ip"))
+        {
+            return int(FM->readJsonFileValue(getFilename(serviceName).c_str(), "ip"));
+        }        
+    }
+    else if(FM->fExist(getFilename(serviceName, true).c_str()))
+    {
+        //service already added - readout File
+        if(FM->checkForKeyInJSONFile(getFilename(serviceName, true).c_str(), "ip"))
+        {
+            return int(FM->readJsonFileValue(getFilename(serviceName, true).c_str(), "ip"));
+        }   
+    }
+    
 
+    #ifdef J54J6_LOGGING_H
+        logger logging;
+        logging.SFLog(className, "getServiceIP", "Can't find ServiceIP - search in network...", 0);
+    #endif
+
+    //if file not exist or key "port" is not defined in File search for service and recreate the File
+    networkIdent->searchForService(serviceName);
+    lastRequest.id = networkIdent->getLastGeneratedId();
+    lastRequest.createdAt = millis();
+    lastRequest.serviceName = serviceName;
+    lastRequest.searchType = 1;
+    return IPAddress(0,0,0,0);
 }
+
+String ExternServiceHandler::getServiceMACAddress(const char* serviceName)
+{
+    if(FM->fExist(getFilename(serviceName).c_str()))
+    {
+        //service already added - readout File
+        if(FM->checkForKeyInJSONFile(getFilename(serviceName).c_str(), "mac"))
+        {
+            return String(FM->readJsonFileValue(getFilename(serviceName).c_str(), "mac"));
+        }        
+    }
+    else if(FM->fExist(getFilename(serviceName, true).c_str()))
+    {
+        //service already added - readout File
+        if(FM->checkForKeyInJSONFile(getFilename(serviceName, true).c_str(), "mac"))
+        {
+            return String(FM->readJsonFileValue(getFilename(serviceName, true).c_str(), "mac"));
+        }   
+    }
+    
+    
+    #ifdef J54J6_LOGGING_H
+        logger logging;
+        logging.SFLog(className, "getServiceIP", "Can't find ServiceMAC - search in network...", 0);
+    #endif
+
+    //if file not exist or key "port" is not defined in File search for service and recreate the File
+    networkIdent->searchForService(serviceName);
+    lastRequest.id = networkIdent->getLastGeneratedId();
+    lastRequest.createdAt = millis();
+    lastRequest.serviceName = serviceName;
+    lastRequest.searchType = 3;
+    return "false";
+}
+
 
 bool ExternServiceHandler::autoAddService(const char* serviceName)
 {
-
+    if(!autoAddWait) //autoAddWait only because of the correct Log Message - theoretically i can check it here...
+    {
+        if(FM->fExist(getFilename(serviceName).c_str()))
+        {
+            #ifdef J54J6_LOGGING_H
+                logger logging;
+                logging.SFLog(className, "autoAddService", "Service Cfg already exist - no autoConf needed! - return true", 1);
+            #endif
+            return true;
+        }
+        else
+        {
+            networkIdent->searchForService(serviceName);
+            lastRequest.id = networkIdent->getLastGeneratedId();
+            lastRequest.createdAt = millis();
+            lastRequest.serviceName = serviceName;
+            lastRequest.searchType = 3;
+            autoAddWait = true;
+            autoAddLastCall = millis();
+        }
+        
+    }
+    else
+    {
+        if(millis() < (lastRequest.createdAt + lastRequest.deleteAfter - 100))
+        {
+            
+            if(millis() > (autoAddLastCall + checkDelay))
+            {
+                autoAddLastCall = millis();
+                if(FM->fExist(getFilename(lastRequest.serviceName).c_str()))
+                {
+                    #ifdef J54J6_LOGGING_H
+                        logger logging;
+                        String message = "Service \"";
+                        message += lastRequest.serviceName;
+                        message += "\" successfully added";
+                        logging.SFLog(className, "autoAddService", message.c_str(), 0);
+                    #endif
+                    lastRequest.reset();
+                    autoAddWait = false;
+                    return true;
+                }
+                return false;
+            }
+            else
+            {
+                delay(10);
+                return false;
+            }
+            
+        }
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            String message = "Service \"";
+            message += lastRequest.serviceName;
+            message += "\" can't be added - timeout";
+            logging.SFLog(className, "autoAddService", message.c_str(), 1);
+        #endif
+        lastRequest.reset();
+        autoAddWait = false;
+        return false;
+    }
+    return false;
 }
 
-bool ExternServiceHandler::manAddService(const char* serviceName, int port, IPAddress ip, bool checkConnect, const char* MAC)
+bool ExternServiceHandler::manAddService(const char* serviceName, int port, IPAddress ip, bool fallback, bool checkConnect, const char* MAC)
 {
-
+   
 }
 
 bool ExternServiceHandler::delService(const char* serviceName)
@@ -54,17 +203,143 @@ bool ExternServiceHandler::delService(const char* serviceName)
 
 
 //helper functions
-String ExternServiceHandler::getFilename(const char* serviceName)
+String ExternServiceHandler::getFilename(const char* serviceName, bool fallback)
 {
     String cacheName = serviceName;
     cacheName.replace(" ", "");
 
     String rfilename = serviceAddressListFile;
     rfilename += cacheName;
+    if(fallback)
+    {
+        rfilename += "-fallback";
+    }
     rfilename += ".json";
+    rfilename.replace(" ", "");
+    return rfilename;
 }
 
 
+
+//loop
+void ExternServiceHandler::loop()
+{
+    if(lastRequest.id != 0) //there is an request < 10 seconds old
+    {
+        StaticJsonDocument<425> lastFetched = networkIdent->getLastData();
+        if(lastFetched.containsKey("type"))
+        {
+            if(lastFetched["type"] == "answer")
+            {
+                if(lastFetched.containsKey("id") && lastFetched.containsKey("serviceName"))
+                {
+                    String m1 = lastFetched["id"];
+                    String m2 = lastFetched["serviceName"];
+                    m1.replace(" ", "");
+                    m2.replace(" ", "");
+
+                    if(m1 == String(lastRequest.id) && m2 == String(lastRequest.serviceName))
+                    {
+                        if(lastFetched.containsKey("ip") && lastFetched.containsKey("mac") && lastFetched.containsKey("servicePort"))
+                        {
+                            //current request has the same id like last fech -> result will be saved in file as new Service address
+                            if(!FM->fExist(getFilename(lastRequest.serviceName).c_str()))
+                            {
+                                //create first Service File
+                                FM->fOpen(getFilename(lastRequest.serviceName).c_str(), "w");
+                                FM->fWrite(" ");
+                                FM->fClose();
+                                FM->appendJsonKey(getFilename(lastRequest.serviceName).c_str(), "ip", lastFetched["ip"]);
+                                FM->appendJsonKey(getFilename(lastRequest.serviceName).c_str(), "mac", lastFetched["mac"]);
+                                FM->appendJsonKey(getFilename(lastRequest.serviceName).c_str(), "port", lastFetched["servicePort"]);
+                                #ifdef J54J6_LOGGING_H
+                                    logger logging;
+                                    String message = "Main Cfg for Service \"";
+                                    message += lastRequest.serviceName;
+                                    message += "\" was added";
+                                    logging.SFLog(className, "loop", message.c_str());
+                                #endif
+                                return;
+                            }
+                            if(!FM->fExist(getFilename(lastRequest.serviceName, true).c_str()))
+                            {
+                                //create Fallback Address File - only generated if manually poked or if first host can't be found
+                                String ipOnFile = FM->readJsonFileValue(getFilename(lastRequest.serviceName).c_str(), "ip");
+                                String fetchedIP = lastFetched["ip"];
+                                ipOnFile.replace(" ","");
+                                fetchedIP.replace(" ", "");
+                                if(ipOnFile == fetchedIP)
+                                {
+                                    #ifdef J54J6_LOGGING_H
+                                        logger logging;
+                                        logging.SFLog(className, "loop", "Can't add new ext. Service Fallback Record - ip already registered as Main", 1);
+                                    #endif
+                                    return;
+                                }
+                                else
+                                {
+                                    FM->fOpen(getFilename(lastRequest.serviceName, true).c_str(), "w");
+                                    FM->fClose();
+                                    FM->appendJsonKey(getFilename(lastRequest.serviceName, true).c_str(), "ip", lastFetched["ip"]);
+                                    FM->appendJsonKey(getFilename(lastRequest.serviceName, true).c_str(), "mac", lastFetched["mac"]);
+                                    FM->appendJsonKey(getFilename(lastRequest.serviceName, true).c_str(), "port", lastFetched["servicePort"]);
+                                    #ifdef J54J6_LOGGING_H
+                                        logger logging;
+                                        String message = "Fallback Cfg for Service \"";
+                                        message += lastRequest.serviceName;
+                                        message += "\" was added";
+                                        logging.SFLog(className, "loop", message.c_str());
+                                    #endif
+                                    return;
+                                }
+                            }
+                            else
+                            {
+                                //Nothing to do - fallback and main are configured - at this time only two ways are supported - updated later by an universal way
+                            }
+                            
+                        }
+                        else
+                        {
+                            //one or more keys are missing
+                            #ifdef J54J6_LOGGING_H
+                                logger logging;
+                                logging.SFLog(className, "loop", "Can't add new ext. Service Record - one ore more keys are missing", 0);
+                            #endif
+                        }
+                    }
+                }
+                else
+                {
+                    //Not useable for this lib - there is no id stored - maybe for another lib or program
+                }
+                
+            }
+            else
+            {
+                //last fetched was an request or non-syntax confirm - skip
+            }
+            
+        }
+        else
+        {
+            //Nothing to do - no "type" in last Fetch - maybe because of other program using networkIdent port or wrong useage of lib
+        }
+    }
+    else 
+    {
+        /*
+            Nothing to do - no requests
+        */
+    }
+    
+
+    if(autoAddWait)
+    {
+        autoAddService();
+    }
+    lastRequest.loop();
+}
 
 /*
     Inherited overwritten functionalities
--- lib/externServiceHandler/src/externServiceHandler.h
+++ lib/externServiceHandler/src/externServiceHandler.h
@@ -4,9 +4,58 @@
 
 
 #include "errorHandler.h"
+#include "logging.h"
 #include "NetworkIdent.h"
 #include "wifiManager.h"
 #include "filemanager.h"
+
+
+/*
+    externalServiceRegistration File Blueprint:
+    Filename-Syntax: <<serviceAddressListFile>>+serviceName+.json
+    =>/config/serviceHandler/registered/mqtt.json
+
+    JSON-Format:
+    {
+        "ip" : "<<Address>>",
+        "mac" : "<<mac>>",
+        "port" : <<port>>,
+
+    }
+*/
+
+struct networkSearchCacheValueHolder {
+    const char* serviceName = "n.S";
+    int searchType = 0; // 0 = nothing, 1 = ip, 2 = port, 3 = mac, 4 - autoAdd
+    long id = 0;
+    ulong createdAt = 0;
+    int deleteAfter = 20000; //10 seconds default delay
+    
+    void reset() {
+        searchType = 0;
+        serviceName = "n.S";
+        id = 0;
+        createdAt = 0;
+
+        #ifdef J54J6_LOGGING_H
+            logger logging;
+            logging.SFLog("Struct - networkSearchCacheValueHolder", "reset", "reset ServiceRequestCacheStruct", 0);
+        #endif
+    }
+
+    void loop()
+    {
+        if(serviceName == "n.S")
+        {
+            return;
+        }
+        if(millis() >= (createdAt + deleteAfter))
+        {
+            reset();
+        }
+    }
+
+};
 
 class ExternServiceHandler : public ErrorSlave {
     private:
@@ -14,22 +63,40 @@ class ExternServiceHandler : public ErrorSlave {
         bool classDisabled = false;
         const char* serviceAddressListFile = "/config/serviceHandler/registered/";
 
+        int maxReponseTime = 30000; //max time an ID(process) is saved in idList - after this delay (in ms) - the id will be deleted and handler does not listen for this package - in this case you need to resend it
+
+
+        //you can handle up to 10 requests at the same "time" - bec. of only one core there is more like you can wait for up to 10 answers of other devices at the same time ^^ (later implemented (in V1.5+))
+        networkSearchCacheValueHolder lastRequest;
+        //int counter = 0;
+
         //handler
         Filemanager* FM;
         WiFiManager* wifiManager;
         NetworkIdent* networkIdent;
+
+
+        //ControlFlow Vars
+        bool autoAddWait = false;
+        ulong autoAddLastCall;
+        int checkDelay = 3000;
 
     public:
         ExternServiceHandler(Filemanager* FM, WiFiManager* wifiManager, NetworkIdent* networkIdent);
 
         int getServiceDestPort(const char* serviceName); //look in configFile for preDefined Service Port if nothing found autoAdd is used
         IPAddress getServiceIP(const char* serviceName); //look in configFile for preDefined Service Address if nothing found autoAdd is used
-        bool autoAddService(const char* serviceName); //search and add service to config if found in network
-        bool manAddService(const char* serviceName, int port, IPAddress ip, bool checkConnect = true, const char* MAC = "n.S"); //manually write Service into File e.g for webInterface 
-        bool delService(const char* serviceName);
+        String getServiceMACAddress(const char* serviceName); //look in configFile for preDefined Service MAC Address if nothing foud autoAdd is used
+        bool autoAddService(const char* serviceName = "n.S"); //search and add service to config if found in network
+        bool manAddService(const char* serviceName = "n.S", int port = -1, IPAddress ip = IPAddress(0,0,0,0), bool fallback = false, bool checkConnect = true, const char* MAC = "n.S"); //manually write Service into File e.g for webInterface 
+        bool delService(const char* serviceName); //delete an external Service manually or if implemented e.g after 3 times of non reach
 
         //helper functions
-        String getFilename(const char* servicename);
+        String getFilename(const char* servicename, bool fallback = false);
+
+
+        //loop
+        void loop();
 
          /*
         Inherited ErrorHandling
