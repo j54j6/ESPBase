577abc1c226f1f68ea1324e6c7088fb1d8183cce
j54j6 <justin-ritter@outlook.com>
2021-02-13 13:25:03 +0100

    TestClass

---
+++ lib/logging/src/externLogger.h
@@ -0,0 +1,23 @@
+#ifndef EXTERNLOGGER_H
+#define EXTERNLOGGER_H
+#include <Arduino.h>
+#include "mqttHandler.h"
+#include "filemanager.h"
+#include "wifiManager.h"
+
+class ExternLogger {
+    private:
+        Filemanager* FM;
+        WiFiManager* wifi;
+        MQTTHandler* mqtt;
+
+        String mqttTopic = "/control/log";
+
+    public:
+        ExternLogger(Filemanager* FM, WiFiManager* wifi, MQTTHandler* mqtt);
+
+        bool logItMQTT(String message);
+        //bool logItMQTT(String function, String message, String logLevel);
+};
+
+#endif //EXTERNLOGGER_H
